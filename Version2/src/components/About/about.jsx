49f1f752ac60b9808d9aac003e5abd02982d7bd9
Michael Freeman <michaeljrfreeman@gmail.com>
2023-02-03 17:25:07 +0000

    Changed The About Paragraph for sm screens

--- Version2/src/components/About/about.jsx
+++ Version2/src/components/About/about.jsx
@@ -43,14 +43,14 @@ const About = () => {
               </article>
             </div>
 
-            <p>
-              Hi, I'm Michael Freeman, a web developer with 1 year of experience in creating stylish and functional websites.<br/>
-              My core skills include HTML and CSS and Python, and I am currently working on expanding my knowledge of JavaScript.<br />
-              In my previous projects, I have honed my skills in crafting clean and user-friendly designs, and I take pride in ensuring my websites are responsive and easy to navigate.<br />
-              I am always eager to learn and improve, and I am excited to bring my passion for web development to new projects and challenges.<br />
-              My portfolio showcases my work on a variety of projects, including personal blogs, small business websites, and online portfolios.<br />
-              I believe in keeping things straightforward and intuitive, and I am always striving to stay up-to-date with the latest trends and technologies in web development.
-            </p>
+              <p>
+                Hi, I'm Michael Freeman, a web developer with 1 year of experience in creating stylish and functional websites.<br/>
+                My core skills include HTML, CSS3, Python and I am currently working on expanding my knowledge of JavaScript.<br /><br />
+                In my previous projects, I have honed my skills in crafting clean and user-friendly designs, and I take pride in ensuring my websites are responsive and easy to navigate.<br /><br />
+                I believe in keeping things straightforward and intuitive, and I am always striving to stay up-to-date with the latest trends and technologies in web development.<br /><br />
+                My portfolio showcases my work on a variety of projects, including personal blogs, small business websites, and online portfolios.
+              </p>
+            
 
             <a href="#contact" className="btn btn-primary">Let's Talk</a>
 
