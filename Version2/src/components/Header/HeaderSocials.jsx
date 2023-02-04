d4a0d55387efaed20ce6cb7d13e95e181ab30768
Michael Freeman <michaeljrfreeman@gmail.com>
2023-02-04 16:06:49 +0000

    Updated LinkedIn href target

--- Version2/src/components/Header/HeaderSocials.jsx
+++ Version2/src/components/Header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import { BsLinkedin, BsGithub, BsDribbble } from "react-icons/bs"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="www.linkedin.com/in/michael-freeman-349016232" target="_blank"> <BsLinkedin /> </a>
+        <a href="https://linkedin.com/in/michael-freeman1902" target="_blank"> <BsLinkedin /> </a>
         <a href="https://github.com/gridline1902" target="_blank"> <BsGithub /> </a>
         <a href="https://dribbble.com/gridline1902" target="_blank"> <BsDribbble /> </a>
     </div>
